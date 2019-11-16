79f63fffb383c75e2e86c3e3a997eab3a2838fc1
Michael Marchand <michaeldavidmarchand@gmail.com>
2019-11-16 07:38:06 -0700

    working Board class

--- lib/my_hangman/board.rb
+++ lib/my_hangman/board.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
-class Board
+module MyHangman
+  class Board
+    attr_accessor :letters
 
-  attr_accessor :letters
+    def initialize(letters = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z])
+      @letters = letters
+    end
 
-  def initialize(letters = '')
-    @letters = letters
+    def display
+      print "#{@letters.join(' ')}\n\n"
+      puts "hello there"
+    end
   end
 end
+
+board = MyHangman::Board.new
+board.display

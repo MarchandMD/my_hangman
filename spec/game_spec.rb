5686d598c49536f0efbda8e803bfa43f26293b89
Michael Marchand <michaeldavidmarchand@gmail.com>
2020-01-01 22:01:28 -0700

    working on game_spec tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,7 +4,12 @@ require 'spec_helper.rb'
 
 module Hangman
   RSpec.describe Game do
-    
+    context '#initialize' do
+      it 'has :solution, :board, :turns, :letter, :words, :bad_guess attributes' do
+        test_game = Game.new
+        expect(test_game).to be_truthy
+      end
+    end
     
   end
 end
